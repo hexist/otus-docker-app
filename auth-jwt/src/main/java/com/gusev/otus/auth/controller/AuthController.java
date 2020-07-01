040d61d9802bbd6fd09080ed40c41d7b4a40d7b0
ai.gusev@bftcom.com <ai.gusev@bftcom.com>
2020-07-01 16:20:44 +0300

    add auth service initial commit

---
+++ auth-jwt/src/main/java/com/gusev/otus/auth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.gusev.otus.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Auth rest controller
+ *
+ * @author A. Gusev
+ * @since 01 июль 2020 г.
+ */
+@RestController
+public class AuthController {
+
+}
